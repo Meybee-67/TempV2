9add21b6be1696a33afb980aca295672a060984a
Meybee-67 <167405254+Meybee-67@users.noreply.github.com>
2024-06-15 14:55:11 +0200

    Add files via upload

---
+++ lib/home-page.dart
@@ -0,0 +1,108 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+
+class MyHomePage extends StatefulWidget{
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  //Get time
+  DateTime now = DateTime.now();
+  int currentYear = DateTime.now().year;
+  String formattedDate = DateFormat.yMMMMEEEEd().format(DateTime.now());
+
+  //Get data
+String time = "";
+bool _isLoading = false;
+String _temperature = '';
+bool hotspot = true;
+
+Future<void> fetchData() async {
+  setState(() {
+    _isLoading = true;
+  });
+    try {
+      final temperature = await http.get(
+        Uri.parse('http://192.168.4.1/data'),
+      );
+      log("this is the response: $temperature");
+
+      if (temperature.statusCode == 200) {
+        setState(() {
+          _temperature = json.decode(temperature.body)['temperature'];
+          _isLoading = false;
+        });
+      } else {
+        setState(() {
+          _temperature = "Failed to fetch data";
+          _isLoading = false;
+          hotspot = false;
+        });
+      }
+    } catch (e) {
+      log("", error: e, name: "error");
+      setState(() {
+        _temperature = "Failed to fetch data";
+        _isLoading = false;
+        hotspot=false;
+      });
+    }
+    _isLoading = false;
+  }
+
+@override
+  void initState() {
+  Timer mytimer = Timer.periodic(Duration(seconds: 5), (timer) {
+        fetchData();
+        time = '$_temperature';
+        setState(() {
+        });
+        //mytimer.cancel() //to terminate this timer
+});
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+    body : Stack(
+      children: <Widget>[
+          const Positioned(
+          top: 60,
+          left : 20,
+          child : Text("Wissembourg", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
+          ),
+        Positioned(
+          top :95,
+          left :20,
+          child : Text(formattedDate,style:const TextStyle(fontSize:15),)),
+        const Positioned(
+          top:140,
+          left :35,
+          child: Icon(
+                FontAwesomeIcons.solidMoon,
+                color: Color.fromARGB(255, 223, 196, 22),
+                size: 100,
+              ),
+        ),
+        Positioned(
+          top:140,
+          right:50,
+          child : Text(time+"°C",style: TextStyle(fontSize: 30))
+        )
+        ]
+    )
+    );
+  }
+}
